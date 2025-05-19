ea47630375cb7f2245145f6c13af11bab7e69ffa
Juan Pablo Ante <juan.pablo.ante@correounivalle.edu.co>
2025-05-19 10:55:44 -0500

    Creating login

--- EcommerceBackend/EcommerceBackend/settings.py
+++ EcommerceBackend/EcommerceBackend/settings.py
@@ -11,9 +11,12 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(BASE_DIR / '.env')
 
 
 # Quick-start development settings - unsuitable for production
@@ -37,6 +40,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'rest_framework_simplejwt.token_blacklist',
+    'corsheaders',
+    'drf_yasg',    
+    'users'
 ]
 
 MIDDLEWARE = [
@@ -72,10 +81,23 @@ WSGI_APPLICATION = 'EcommerceBackend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+ALLOWED_HOSTS = ['127.0.0.1','localhost', os.environ.get("DATABASE_HOST", "")]
+
+# Agrega dominios locales cuando DEBUG=True
+if DEBUG:
+    ALLOWED_HOSTS += ["localhost", "127.0.0.1"]
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get("DB_NAME", ""),
+        'USER': os.environ.get("DB_USER", ""),
+        'PASSWORD': os.environ.get("PASSWORD", ""),
+        'HOST': os.environ.get("DATABASE_HOST", ""),  # o el host de tu servidor de PostgreSQL
+        'PORT': os.environ.get("DATABASE_PORT", "5432"),      # el puerto predeterminado de PostgreSQL
+        'OPTIONS': {
+            'sslmode': 'require',  # Obliga el uso de SSL
+        },
     }
 }
 
@@ -120,3 +142,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+AUTH_USER_MODEL = 'users.Usuario'
