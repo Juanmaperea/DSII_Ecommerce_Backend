f72911f85fd2a7db1fd7c930de0f245be2568594
David Enciso <david.enciso@correounivalle.edu.co>
2025-06-10 18:29:02 -0500

    Adding Corrected New Unit Tests Backend

--- EcommerceBackend/users/tests/test_models.py
+++ EcommerceBackend/users/tests/test_models.py
@@ -1,4 +1,8 @@
-import pytest
+import pytest 
+
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
 from django.db import IntegrityError
 from django.core.exceptions import ValidationError
 from django.test import TestCase
