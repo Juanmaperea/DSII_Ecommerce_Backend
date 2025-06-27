927d1938ce357c42599b603df5610429b572c220
Juanmaperea <53235315+Juanmaperea@users.noreply.github.com>
2025-06-27 12:16:28 -0500

    Update settings.py

--- EcommerceBackend/EcommerceBackend/settings.py
+++ EcommerceBackend/EcommerceBackend/settings.py
@@ -162,12 +162,10 @@ LOGOUT_REDIRECT_URL = '/'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
-    ],
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
 
     'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
